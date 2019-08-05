991003d80057943b0aec9b08841123b38bf3ebbc
Quang Le <ducquang98@gmail.com>
2019-08-05 12:41:32 +0700

    refactor + add specs

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -3,7 +3,7 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
       if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > 0
@@ -50,6 +50,10 @@ class GildedRose
       end
     end
   end
+
+  def refactored_update_quality
+    @items.each(&:update_quality)
+  end
 end
 
 class Item
@@ -61,7 +65,85 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+
+  def update_quality
+    adjust_quality
+    update_sell_in
+  end
+
+  def update_sell_in
+    self.sell_in = sell_in - 1
+  end
+
+  def adjust_quality
+    new_quality = quality - 1
+    new_quality -= 1 if sell_in < 0
+    self.quality = [new_quality, 0].max
+  end
+end
+
+class AgedBrieItem < Item
+  NAME = 'Aged Brie'
+
+  def initialize(sell_in, quality)
+    super(NAME, sell_in, quality)
+  end
+
+  def adjust_quality
+    new_quality = quality + 1
+    new_quality += 1 if sell_in < 0
+    self.quality = [new_quality, 50].min
+  end
+end
+
+class SulfurasItem < Item
+  NAME = 'Sulfuras, Hand of Ragnaros'
+
+  def initialize(sell_in, quality)
+    super(NAME, sell_in, quality)
+  end
+
+  def adjust_quality
+  end
+
+  def update_sell_in
+  end
+end
+
+class BackstagePassesItem < Item
+  NAME = 'Backstage passes to a TAFKAL80ETC concert'
+
+  def initialize(sell_in, quality)
+    super(NAME, sell_in, quality)
+  end
+
+  def adjust_quality
+    if sell_in < 0
+      self.quality = 0
+    elsif sell_in < 6
+      self.quality = [quality + 3, 50].min
+    elsif sell_in < 11
+      self.quality = [quality + 2, 50].min
+    else
+      self.quality = [quality + 1, 50].min
+    end
+  end
+end
+
+class ItemFactory
+  def self.create(name, sell_in, quality)
+    case name
+    when AgedBrieItem::NAME
+      AgedBrieItem.new(sell_in, quality)
+    when SulfurasItem::NAME
+      SulfurasItem.new(sell_in, quality)
+    when BackstagePassesItem::NAME
+      BackstagePassesItem.new(sell_in, quality)
+    else
+      Item.new(name, sell_in, quality)
+    end
   end
 end
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,11 +1,183 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'byebug'
 
 describe GildedRose do
-  describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+  [:update_quality, :refactored_update_quality].each do |method_name|
+    describe "##{method_name}" do
+      let(:sell_in) { 10 }
+      let(:quality) { 40 }
+      let(:item) { ItemFactory.create(item_name, sell_in, quality) }
+      let(:items) { [item] }
+
+      context 'when item is normal' do
+        let(:item_name) { 'foo' }
+
+        it 'does not change the name' do
+          expect {
+            GildedRose.new(items).send(method_name)
+          }.not_to change { item.name }
+        end
+
+        context 'when sell_in is positive' do
+          context 'when quality is positive' do
+            it 'decrease quality by 1 and sell_in by 1' do
+              GildedRose.new(items).send(method_name)
+              expect(item.sell_in).to eq (sell_in - 1)
+              expect(item.quality).to eq (quality - 1)
+            end
+          end
+
+          context 'when quality is zero' do
+            let(:quality) { 0 }
+
+            it 'decrease quality to 0 at most and and decrease sell_in by 1' do
+              GildedRose.new(items).send(method_name)
+              expect(item.sell_in).to eq (sell_in - 1)
+              expect(item.quality).to eq quality
+            end
+          end
+        end
+
+        context 'when sell_in is negative' do
+          let(:sell_in) { -1 }
+
+          context 'when quality is greater than 1' do
+            it 'decrease quality by 2 and sell_in by 1' do
+              GildedRose.new(items).send(method_name)
+              expect(item.sell_in).to eq (sell_in - 1)
+              expect(item.quality).to eq (quality - 2)
+            end
+          end
+
+          context 'when quality is smaller than 1' do
+            let(:quality) { 1 }
+
+            it 'decrease quality to 0 at most and and decrease sell_in by 1' do
+              GildedRose.new(items).send(method_name)
+              expect(item.sell_in).to eq (sell_in - 1)
+              expect(item.quality).to eq 0
+            end
+          end
+        end
+      end
+
+      context 'when item is Aged Brie' do
+        let(:item_name) { AgedBrieItem::NAME }
+
+        context 'when sell_in is positive' do
+          context 'when quality is smaller than 50' do
+            it 'increases quantity by 1 and decreases sell_in by 1' do
+              GildedRose.new(items).send(method_name)
+              expect(item.sell_in).to eq (sell_in - 1)
+              expect(item.quality).to eq (quality + 1)
+            end
+          end
+
+          context 'when quality is equal to 50' do
+            let(:quality) { 50 }
+
+            it 'keeps quality and decreases sell_in by 1' do
+              GildedRose.new(items).send(method_name)
+              expect(item.sell_in).to eq (sell_in - 1)
+              expect(item.quality).to eq quality
+            end
+          end
+        end
+
+        context 'when sell_in is negative' do
+          let(:sell_in) { -1 }
+
+          context 'when quality is greater than 1' do
+            it 'increases quantity by 2 and decreases sell_in by 1' do
+              GildedRose.new(items).send(method_name)
+              expect(item.sell_in).to eq (sell_in - 1)
+              expect(item.quality).to eq (quality + 2)
+            end
+          end
+        end
+      end
+
+      context 'when item is Backstage passes' do
+        let(:item_name) { BackstagePassesItem::NAME }
+
+        context 'when sell_in is positive' do
+          context 'when quality is smaller than 50' do
+            context 'when sell_in is greater than 10' do\
+              let(:sell_in) { 11 }
+
+              it 'increases quantity by 1 and decreases sell_in by 1' do
+                GildedRose.new(items).send(method_name)
+                expect(item.sell_in).to eq (sell_in - 1)
+                expect(item.quality).to eq (quality + 1)
+              end
+            end
+
+            context 'when sell_in is greater than 5' do
+              let(:sell_in) { 6 }
+
+              it 'increases quantity by 2 and decreases sell_in by 1' do
+                GildedRose.new(items).send(method_name)
+                expect(item.sell_in).to eq (sell_in - 1)
+                expect(item.quality).to eq (quality + 2)
+              end
+            end
+
+            context 'when sell_in is less than 5' do
+              let(:sell_in) { 3 }
+
+              it 'increases quantity by 3 and decreases sell_in by 1' do
+                GildedRose.new(items).send(method_name)
+                expect(item.sell_in).to eq (sell_in - 1)
+                expect(item.quality).to eq (quality + 3)
+              end
+            end
+          end
+
+          context 'when quality is equal to 50' do
+            let(:quality) { 50 }
+
+            it 'keeps quality and decreases sell_in by 1' do
+              GildedRose.new(items).send(method_name)
+              expect(item.sell_in).to eq (sell_in - 1)
+              expect(item.quality).to eq quality
+            end
+          end
+        end
+
+        context 'when sell_in is negative' do
+          let(:sell_in) { -1 }
+
+          context 'when quality is greater than 1' do
+            it 'sets quantity to 0 and decreases sell_in by 1' do
+              GildedRose.new(items).send(method_name)
+              expect(item.sell_in).to eq (sell_in - 1)
+              expect(item.quality).to eq 0
+            end
+          end
+        end
+      end
+
+      context 'when item is Sulfuras' do
+        let(:item_name) { SulfurasItem::NAME }
+
+        context 'when sell_in is positive' do
+          it 'does not change quality and sell_in' do
+            GildedRose.new(items).update_quality
+            expect(item.sell_in).to eq sell_in
+            expect(item.quality).to eq quality
+          end
+        end
+
+        context 'when sell_in is negative' do
+          let(:sell_in) { -1 }
+
+          it 'does not change quality and sell_in' do
+            GildedRose.new(items).update_quality
+            expect(item.sell_in).to eq sell_in
+            expect(item.quality).to eq quality
+          end
+        end
+      end
     end
   end
 end
